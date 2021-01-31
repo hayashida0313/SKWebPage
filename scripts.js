cfe0ca2dcf47d51ab5d517550989b7a180d187c2
hayashida0313 <56869453+hayashida0313@users.noreply.github.com>
2021-01-31 18:38:35 +0900

    WebPageUpgrade1

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
-/**/
+function loaded() {
+    document.getElementById("loading").classList.remove("active");
+}
+
+/* TODO: 関数にしたいがなぜか TimeOut が効かなくなる・・ 
+function showCharactor(id) {
+    document.getElementById(id).classList.add("show");
+}
+*/
+
+window.addEventListener("load", function() {
+    setTimeout(loaded, 1000);
+    setTimeout(function(){document.getElementById("hachiman").classList.add("show")}, 1500);
+    setTimeout(function(){document.getElementById("yui").classList.add("show")}, 1800);
+    setTimeout(function(){document.getElementById("yukino").classList.add("show")}, 2000);
+})
+
+setTimeout(loaded, 5000);
+setTimeout(function(){document.getElementById("hachiman").classList.add("show")}, 6500);
+setTimeout(function(){document.getElementById("yui").classList.add("show")}, 6800);
+setTimeout(function(){document.getElementById("yukino").classList.add("show")}, 7000);
