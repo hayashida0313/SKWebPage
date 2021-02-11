d2709a9f2403a00b47fe12dfa8e2f32a6d6baf43
k-russs <56869453+k-russs@users.noreply.github.com>
2021-02-11 22:17:40 +0900

    ZeroEdition

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 function loaded() {
-    document.getElementById("loading").classList.remove("active");
+    document.getElementById("loading").classList.add("loaded");
+    document.getElementById("loader").classList.add("loaded");
 }
 
 /* TODO: 関数にしたいがなぜか TimeOut が効かなくなる・・ 
@@ -10,12 +11,23 @@ function showCharactor(id) {
 
 window.addEventListener("load", function() {
     setTimeout(loaded, 1000);
-    setTimeout(function(){document.getElementById("hachiman").classList.add("show")}, 1500);
-    setTimeout(function(){document.getElementById("yui").classList.add("show")}, 1800);
-    setTimeout(function(){document.getElementById("yukino").classList.add("show")}, 2000);
+    setTimeout(function(){document.getElementById("hachiman").classList.add("show")}, 1800);
+    setTimeout(function(){document.getElementById("yui").classList.add("show")}, 2000);
+    setTimeout(function(){document.getElementById("yukino").classList.add("show")}, 2100);
 })
 
 setTimeout(loaded, 5000);
 setTimeout(function(){document.getElementById("hachiman").classList.add("show")}, 6500);
 setTimeout(function(){document.getElementById("yui").classList.add("show")}, 6800);
 setTimeout(function(){document.getElementById("yukino").classList.add("show")}, 7000);
+
+
+const targetElement = document.querySelectorAll(".posts");
+document.addEventListener("scroll", function() {
+    for(let i = 0; i < targetElement.length; i++) {
+        const getElementDistance = targetElement[i].getBoundingClientRect().top + targetElement[i].clientHeight * .3
+        if (window.innerHeight > getElementDistance) {
+            targetElement[i].classList.add("show");
+        }
+    }
+})
